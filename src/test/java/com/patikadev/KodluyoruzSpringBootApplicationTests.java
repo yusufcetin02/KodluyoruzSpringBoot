92438469d77753a5dc3502fe584de35e862d0f5b
yusufcetin02 <yusufcetin@stu.aydin.edu.tr>
2022-07-15 17:23:04 +0300

    First commit

---
+++ src/test/java/com/patikadev/KodluyoruzSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patikadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
